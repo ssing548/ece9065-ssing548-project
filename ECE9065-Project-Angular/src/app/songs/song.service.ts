410a70916fd18ad2c420c10cb739b94cc5f59d38
ssing548 <55406432+ssing548@users.noreply.github.com>
2019-11-22 16:33:50 -0500

    songs service implemented to fetch songs from local songs json

---
+++ ECE9065-Project-Angular/src/app/songs/song.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ISong } from './songs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SongService {
+  private songUrl = 'assets/songs.json';
+
+  constructor(private http: HttpClient) { }
+
+  getSongs(): Observable<ISong[]>{
+    return this.http.get<ISong[]>(this.songUrl)
+      .pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data))),
+        map(data => data.sort((a,b)=> b.averageRating - a.averageRating))
+       
+      );
+  }
+
+  
+}
